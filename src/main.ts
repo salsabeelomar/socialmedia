7a51e8cd67b6531dbdff163dd5462601f3eacb31
salsabeelomar <omarsalsabeel65@gmail.com>
2023-07-19 11:07:36 +0300

    add Prefix

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,7 @@ import { ResponseInterceptor } from './app.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new ResponseInterceptor());
+  app.setGlobalPrefix('api/v1')
   await app.listen(3000);
 }
 bootstrap();
